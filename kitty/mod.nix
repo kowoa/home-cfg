d58db4d7cc173cc1dfc4d9765a4112957530b014
kowoa <koadahoakodah@gmail.com>
2023-09-28 00:20:51 -0400

    Add bash aliases for kitty modularly

--- kitty/mod.nix
+++ kitty/mod.nix
@@ -1,8 +1,15 @@
 {pkgs, ...}: {
   xdg.configFile."kitty" = {
-    enable = true;
     recursive = true;
     source = ./cfg;
     target = "kitty";
+  };
+
+  xdg.configFile."kitty_aliases" = {
+    target = "bash/aliases/.kitty";
+    text = ''
+      alias icat="kitty +kitten icat"
+      alias ssh="kitty +kitten ssh"
+    '';
   };
 }
