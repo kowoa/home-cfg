1925961826c243401bfbac5e86f7aee7aff3bc33
kowoa <koadahoakodah@gmail.com>
2023-10-01 02:53:50 -0400

    Use delta as git diff pager; Autostart zellij in fish

--- shell/fish.nix
+++ shell/fish.nix
@@ -10,6 +10,20 @@
     enable = true;
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
+
+      # Autostart zellij
+      if begin;
+        command -q zellij;
+        and not set -q ZELLIJ;
+        and test "$TERM_PROGRAM" != "vscode";
+      end;
+        # Attach to the oldest session (last in the list) if available
+        zellij attach --create $(zellij ls | tail -n 1)
+      end
+
+      if command -q pfetch
+        pfetch
+      end
     '';
   };
 }
