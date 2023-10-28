8eac5babe0770a1d5f1e7d79115b586f1804321c
kowoa <koadahoakodah@gmail.com>
2023-10-28 14:35:59 -0400

    Add .clang-format file in home directory

--- misc/mod.nix
+++ misc/mod.nix
@@ -3,4 +3,13 @@
     source = ./code-flags.conf;
     target = "code-flags.conf";
   };
+
+  home.file = {
+    ".clang-format".text = ''
+      BasedOnStyle: LLVM
+      IndentWidth: 4
+      ColumnLimit: 80
+      AlignAfterOpenBracket: AlwaysBreak
+    '';
+  };
 }
