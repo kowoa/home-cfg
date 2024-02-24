2bb55868097e13b54375fae48a66a41b7d6d1457
kowoa <koadahoakodah@gmail.com>
2024-02-24 03:12:35 -0500

    Change wezterm and zellij themes to catppuccin

--- wezterm/cfg/wezterm.lua
+++ wezterm/cfg/wezterm.lua
@@ -2,31 +2,32 @@ local wezterm = require("wezterm")
 local fonts = require("fonts")
 
 return {
-  -- Fonts
-  font = fonts.font,
-  font_rules = fonts.font_rules,
-  font_size = fonts.font_size,
+	-- Fonts
+	font = fonts.font,
+	font_rules = fonts.font_rules,
+	font_size = fonts.font_size,
 
-  -- Cursor
-  default_cursor_style = "BlinkingBlock",
-  animation_fps = 1,
-  cursor_blink_ease_in = "Constant",
-  cursor_blink_ease_out = "Constant",
-  cursor_blink_rate = 500,
+	-- Cursor
+	default_cursor_style = "BlinkingBlock",
+	animation_fps = 1,
+	cursor_blink_ease_in = "Constant",
+	cursor_blink_ease_out = "Constant",
+	cursor_blink_rate = 500,
 
-  window_background_opacity = 0.95,
-  warn_about_missing_glyphs = false,
-  hide_tab_bar_if_only_one_tab = true,
+	window_background_opacity = 0.95,
+	warn_about_missing_glyphs = false,
+	hide_tab_bar_if_only_one_tab = true,
 
-  color_scheme = "kanagawabones",
+	--color_scheme = "kanagawabones",
+	color_scheme = "Catppuccin Mocha",
 
-  keys = {
-    {
-      key = "t",
-      mods = "CTRL|SHIFT",
-      action = wezterm.action.SpawnCommandInNewTab {
-        args = { "bash" },
-      },
-    }
-  },
+	keys = {
+		{
+			key = "t",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SpawnCommandInNewTab({
+				args = { "bash" },
+			}),
+		},
+	},
 }
